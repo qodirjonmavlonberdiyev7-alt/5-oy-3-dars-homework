54cf8384be10327d92c95c37550f1803c07ef7a4
Qodirjon <qodirjonmavlonberdiyev7@gmail.com>
2025-12-19 15:10:07 +0500

    added

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ db.createCollection("orders")
 //add documents
 
 
-db.books.insertOne({})
+db.books.insertOne({author: "Abdulla Qodiriy",bookName: "O'tgan kunlar", janr: "Roman", year: "1925-1926",nusxa:6})
+db.books.insertOne({author: "Asqad Muxtor",bookName:"Chinor",janr:"Roman",year:"1973",nusxa:45})
+db.books.insertOne({author: "Cho'lpon",bookName:"Kecha va kunduz",janr:" Tarixiy Roman",year:"1936",nusxa:25})
+db.books.insertOne({author: "Odil Yoqubov",bookName:"Ulug‘bek xazinasi",janr:"Tarixiy Roman",year:"1973",nusxa:36})
 
+
+db.
 
 
